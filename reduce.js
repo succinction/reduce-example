02bbb9f47e1d2841ee54a7ec255af13116cf818f
unknown <succinction@gmail.com>
2020-04-12 19:50:48 -0700

    first commit

---
+++ reduce.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const output = fs.readFileSync('data.txt', 'utf8')
+    .trim()
+    .split('\r\n')
+    .map(line => line.split(';'))
+    .reduce((customers, line) => {
+        console.log(JSON.stringify(customers))
+        customers[line[0]] = customers[line[0]] || []
+        customers[line[0]].push({
+            name: line[1],
+            price: line[2],
+            quantity: line[3],
+        })
+        return customers
+    }, {})
+
+console.log('output', output)
+
+

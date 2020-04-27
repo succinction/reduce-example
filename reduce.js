2488e28b769b934bfc7f084ed1ea6dd1bb3ecee9
unknown <succinction@gmail.com>
2020-04-26 19:45:11 -0700

    add console logs

--- reduce.js
+++ reduce.js
@@ -5,7 +5,10 @@ const output = fs.readFileSync('data.txt', 'utf8')
     .split('\r\n')
     .map(line => line.split(';'))
     .reduce((customers, line) => {
-        console.log(JSON.stringify(customers))
+        console.log()
+        console.log(customers)
+        console.log()
+        console.log(JSON.stringify(customers, null, 2))
         customers[line[0]] = customers[line[0]] || []
         customers[line[0]].push({
             name: line[1],
